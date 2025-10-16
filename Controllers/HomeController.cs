c54e1e645123d5436d466bb38f24bb3ddd4465e5
suatii06 <suatrussell@gmail.com>
2025-10-16 10:09:40 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using ActivityManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivityManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
